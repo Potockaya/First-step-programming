9467fd8401e3864033e6b5f6a55c9d50abb7dc02
«Marina <marina1904@list.ru>
2022-07-10 00:57:03 +0300

    Add second solution for Task3

--- SolutionTask3/Program.cs
+++ SolutionTask3/Program.cs
@@ -4,14 +4,20 @@ if(inputLine != null)
 {
     int inputNumber = int.Parse(inputLine);
 
-    int startNumber = (inputNumber * -1);
-
-    string lineOutPut = "";
-    while (startNumber < inputNumber)
+   // int startNumber = (inputNumber * -1);
+    int startNumber = 1;
+   // string lineOutPut = "";
+    string lineOutPut = "0";
+    // while (startNumber < inputNumber)
+    // {
+    //     lineOutPut = lineOutPut + startNumber+", ";
+    //     startNumber++;
+    // }
+    // lineOutPut = lineOutPut + inputNumber;
+    while (startNumber <= inputNumber)
     {
-        lineOutPut = lineOutPut + startNumber+", ";
-        startNumber++;
+       lineOutPut = startNumber*(-1) + ", "+lineOutPut+", "+startNumber; 
+       startNumber++;
     }
-    lineOutPut = lineOutPut + inputNumber;
    Console.WriteLine(lineOutPut);
 }
