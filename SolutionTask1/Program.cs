5e574f88ad8908d339a0d550f82b01f1e94a8ff7
«Marina <marina1904@list.ru>
2022-07-09 23:21:17 +0300

    Second solution for Task1

--- SolutionTask1/Program.cs
+++ SolutionTask1/Program.cs
@@ -5,9 +5,18 @@ if(inputLkneOne != null && inputLkneTwo != null){
     int inputNumberOne = int.Parse(inputLkneOne);
     int inputNumberTwo = int.Parse(inputLkneTwo);
 
-    if(inputNumberOne == inputNumberTwo * inputNumberTwo){
+    // if(inputNumberOne == inputNumberTwo * inputNumberTwo){
+    //     Console.WriteLine("yes");
+    // }else{
+    //     Console.WriteLine("no");
+    // }
+
+    if(inputNumberTwo == Math.Sqrt(inputNumberOne))
+    {
         Console.WriteLine("yes");
-    }else{
+    }
+    else
+    {
         Console.WriteLine("no");
     }
 
