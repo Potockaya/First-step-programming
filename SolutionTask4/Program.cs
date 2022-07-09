8159b103084dac9e777fff3eadfdb4e406bee965
«Marina <marina1904@list.ru>
2022-07-10 01:15:16 +0300

    Add second solution for Task4

--- SolutionTask4/Program.cs
+++ SolutionTask4/Program.cs
@@ -1,10 +1,16 @@
-﻿string? inputLine = Console.ReadLine();
+﻿// string? inputLine = Console.ReadLine();
 
-if(inputLine != null)
-{
-    int inputNumber = int.Parse(inputLine);
+// if(inputLine != null)
+// {
+//     int inputNumber = int.Parse(inputLine);
 
-    int outputNumber = inputNumber%100;
+//     int outputNumber = inputNumber%100;
    
-    Console.WriteLine(outputNumber);
+//     Console.WriteLine(outputNumber);
+// }
+string? inputLine = Console.ReadLine();
+if (inputLine !=null)
+{
+    string lastNum = inputLine.Substring(2,1);
+    Console.WriteLine(lastNum);
 }
