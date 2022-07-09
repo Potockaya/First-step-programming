ae8e84d53ac0ba093f129069327f4e979130cf24
«Marina <marina1904@list.ru>
2022-07-09 23:43:24 +0300

    Add second solution (switch) for Task2

--- SolutionTask2/Program.cs
+++ SolutionTask2/Program.cs
@@ -2,26 +2,47 @@
 
 if(inputLine != null)
 {
-//    switch (inputNumber)
-//    {
-//        case 1:
-//        Consolr.WriteLine("Понедельник");
-//        break;
+   int inputNumber = int.Parse(inputLine);
+   switch (inputNumber)
+   {
+       case 1:
+       Console.WriteLine("Понедельник");
+       break;
 
-//        case 2:
-//        Consolr.WriteLine("Вторник");
-//        break;
-//    }
-    int inputNumber = int.Parse(inputLine);
-    string[] dayofWeek = new string[7];
-    dayofWeek[0] = "Понедельник";
-    dayofWeek[1] = "Вторник";
-    dayofWeek[2] = "Среда";
-    dayofWeek[3] = "Четверг";
-    dayofWeek[4] = "Пятница";
-    dayofWeek[5] = "Суббота";
-    dayofWeek[6] = "Воскресенье";
+       case 2:
+       Console.WriteLine("Вторник");
+       break;
+
+       case 3:
+       Console.WriteLine("Среда");
+       break;
+
+       case 4:
+       Console.WriteLine("Четверг");
+       break;
+
+       case 5:
+       Console.WriteLine("Пятница");
+       break;
+
+       case 6:
+       Console.WriteLine("Суббота");
+       break;
+
+       case 7:
+       Console.WriteLine("Воскресенье");
+       break;
+   }
+    // int inputNumber = int.Parse(inputLine);
+    // string[] dayofWeek = new string[7];
+    // dayofWeek[0] = "Понедельник";
+    // dayofWeek[1] = "Вторник";
+    // dayofWeek[2] = "Среда";
+    // dayofWeek[3] = "Четверг";
+    // dayofWeek[4] = "Пятница";
+    // dayofWeek[5] = "Суббота";
+    // dayofWeek[6] = "Воскресенье";
 
 
-    Console.WriteLine(dayofWeek[inputNumber-1]);
+   // Console.WriteLine(dayofWeek[inputNumber-1]);
 }
