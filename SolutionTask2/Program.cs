6d071e430d3b5661b29f16dc6a5ca72d88e28046
«Marina <marina1904@list.ru>
2022-07-10 00:42:37 +0300

    Add solution 3 for Task2 (dont work)

--- SolutionTask2/Program.cs
+++ SolutionTask2/Program.cs
@@ -3,46 +3,48 @@
 if(inputLine != null)
 {
    int inputNumber = int.Parse(inputLine);
-   switch (inputNumber)
-   {
-       case 1:
-       Console.WriteLine("Понедельник");
-       break;
+   string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(inputNumber));
+   Console.WriteLine(outDayOfWeek);
+//    switch (inputNumber)
+//    {
+//        case 1:
+//        Console.WriteLine("Понедельник");
+//        break;
 
-       case 2:
-       Console.WriteLine("Вторник");
-       break;
+//        case 2:
+//        Console.WriteLine("Вторник");
+//        break;
 
-       case 3:
-       Console.WriteLine("Среда");
-       break;
+//        case 3:
+//        Console.WriteLine("Среда");
+//        break;
 
-       case 4:
-       Console.WriteLine("Четверг");
-       break;
+//        case 4:
+//        Console.WriteLine("Четверг");
+//        break;
 
-       case 5:
-       Console.WriteLine("Пятница");
-       break;
+//        case 5:
+//        Console.WriteLine("Пятница");
+//        break;
 
-       case 6:
-       Console.WriteLine("Суббота");
-       break;
+//        case 6:
+//        Console.WriteLine("Суббота");
+//        break;
 
-       case 7:
-       Console.WriteLine("Воскресенье");
-       break;
-   }
-    // int inputNumber = int.Parse(inputLine);
-    // string[] dayofWeek = new string[7];
-    // dayofWeek[0] = "Понедельник";
-    // dayofWeek[1] = "Вторник";
-    // dayofWeek[2] = "Среда";
-    // dayofWeek[3] = "Четверг";
-    // dayofWeek[4] = "Пятница";
-    // dayofWeek[5] = "Суббота";
-    // dayofWeek[6] = "Воскресенье";
+//        case 7:
+//        Console.WriteLine("Воскресенье");
+//        break;
+//    }
+//     // int inputNumber = int.Parse(inputLine);
+//     // string[] dayofWeek = new string[7];
+//     // dayofWeek[0] = "Понедельник";
+//     // dayofWeek[1] = "Вторник";
+//     // dayofWeek[2] = "Среда";
+//     // dayofWeek[3] = "Четверг";
+//     // dayofWeek[4] = "Пятница";
+//     // dayofWeek[5] = "Суббота";
+//     // dayofWeek[6] = "Воскресенье";
 
 
-   // Console.WriteLine(dayofWeek[inputNumber-1]);
+//    // Console.WriteLine(dayofWeek[inputNumber-1]);
 }
