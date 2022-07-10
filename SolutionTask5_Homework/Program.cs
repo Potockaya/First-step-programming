e7e9dbc01684f267a6611a1fdeeec66124c3211f
«Marina <marina1904@list.ru>
2022-07-10 16:50:49 +0300

    Add solution for Homework Task5

---
+++ SolutionTask5_Homework/Program.cs
@@ -0,0 +1,22 @@
+﻿string inputLine1 = Console.ReadLine();
+string inputLine2 = Console.ReadLine();
+int max = 0;
+if (inputLine1 != null && inputLine2 != null){
+    int inputNumber1 = int.Parse(inputLine1);
+    int inputNumber2 = int.Parse(inputLine2);
+
+    if (inputNumber1 > inputNumber2)
+
+    {
+    max = inputNumber1;
+    Console.Write("max= ");
+    Console.WriteLine(max);
+    }
+    else
+    {
+    max = inputNumber2;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    }
+}
+
