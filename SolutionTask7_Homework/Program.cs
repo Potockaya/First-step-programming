4818e89910a4eb8f6c2f8f25e18685f88b450f1e
«Marina <marina1904@list.ru>
2022-07-10 23:39:37 +0300

    Add solution for Homework Task7

---
+++ SolutionTask7_Homework/Program.cs
@@ -0,0 +1,18 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    if (inputNumber %2 == 0)
+    {Console.WriteLine(inputNumber); 
+    Console.Write("да");
+    
+    }
+    else
+    {Console.WriteLine(inputNumber); 
+    Console.Write("нет");
+    
+    }
+    
+}
