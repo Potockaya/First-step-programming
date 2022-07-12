ee3ced1d2b65c5a279370fa099021a55c1897b44
«Marina <marina1904@list.ru>
2022-07-12 23:06:53 +0300

    Add solution for Task8 Homework

---
+++ SolutionTask8_Homework/Program.cs
@@ -0,0 +1,16 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    string lineOutPut = "";
+    int startNumber = 2;
+    while (startNumber <= inputNumber)
+    {
+        lineOutPut = lineOutPut + startNumber + ", ";
+        startNumber=startNumber+2;
+    }
+ string lineOutPut2 = lineOutPut.TrimEnd(' ');
+Console.WriteLine(lineOutPut2.TrimEnd(','));
+}
