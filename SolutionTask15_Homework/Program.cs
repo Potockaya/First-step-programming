952aff42f157338a8a4b4953bfa9374c54ee9f57
«Marina <marina1904@list.ru>
2022-07-16 20:16:36 +0300

    Add solution for Task 15 Homework

---
+++ SolutionTask15_Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int WeekNumber = int.Parse(Console.ReadLine());
+if ( WeekNumber == 6 || WeekNumber == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    if (WeekNumber > 7 || WeekNumber <= 0)
+{
+    Console.WriteLine("Нет такого дня недели");
+}
+    else{
+    Console.WriteLine("Рабочий день");
+    }
+    
+}
