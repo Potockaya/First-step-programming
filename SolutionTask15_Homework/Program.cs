52266bf1ba4581e14502cbbecc806cccdaaefdea
«Marina <marina1904@list.ru>
2022-07-16 20:29:29 +0300

    Add second solution for Task 15 Homework

--- SolutionTask15_Homework/Program.cs
+++ SolutionTask15_Homework/Program.cs
@@ -1,17 +1,40 @@
-﻿Console.Clear();
-int WeekNumber = int.Parse(Console.ReadLine());
-if ( WeekNumber == 6 || WeekNumber == 7)
+﻿//  1 вариант
+
+// Console.Clear();
+// int WeekNumber = int.Parse(Console.ReadLine());
+// if ( WeekNumber == 6 || WeekNumber == 7)
+// {
+//     Console.WriteLine("Выходной");
+// }
+// else
+// {
+//     if (WeekNumber > 7 || WeekNumber <= 0)
+// {
+//     Console.WriteLine("Нет такого дня недели");
+// }
+//     else{
+//     Console.WriteLine("Рабочий день");
+//     }
+
+// }
+
+//2  вариант
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
 {
-    Console.WriteLine("Выходной");
-}
-else
-{
-    if (WeekNumber > 7 || WeekNumber <= 0)
-{
-    Console.WriteLine("Нет такого дня недели");
-}
-    else{
-    Console.WriteLine("Рабочий день");
+    int inputNumber = int.Parse(inputLine);
+    string outDayOfWeek = System.Globalization.CultureInfo
+        .GetCultureInfo("ru-RU")
+        .DateTimeFormat.GetDayName(
+            (DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(inputNumber)
+        );
+    if (inputNumber == 5 || inputNumber == 6)
+    {
+        Console.WriteLine(outDayOfWeek +" это выходной день");
     }
-    
+    else
+    {
+       Console.WriteLine(outDayOfWeek + " это рабочий день");
+    }
 }
