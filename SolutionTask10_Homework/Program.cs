712db631ec15abb72b2d38084a68a93e3b8700f0
«Marina <marina1904@list.ru>
2022-07-16 19:35:51 +0300

    Add  solution for task 10 Homework

---
+++ SolutionTask10_Homework/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+void middleDigit() 
+{
+    int number = new Random().Next(100, 1000);
+    Console.Write("Случайное трехзначное число : ");
+    Console.WriteLine(number);
+
+    int secondnum = (number % 100)/10;
+    
+    Console.Write("Вторая цифра: ");
+    Console.Write(secondnum);        
+}
+
+middleDigit();
