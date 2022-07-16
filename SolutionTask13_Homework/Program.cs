37cfe8e3fcec38b2a2a4ff3761e87e3e14a8a4a9
«Marina <marina1904@list.ru>
2022-07-16 21:12:37 +0300

    Add second solution for Task 13 Homework

--- SolutionTask13_Homework/Program.cs
+++ SolutionTask13_Homework/Program.cs
@@ -19,3 +19,22 @@ if (inputLine != null)
 
 }
 thirdnumber();
+
+// 2 вариант
+// Console.Clear();
+// void thirdnumber()
+// {
+// int number = int.Parse(Console.ReadLine());
+// int number2 = (number%10%10);
+// int number3 = (number/100);
+
+// if ( number3 == 0)
+//     {
+//         Console.WriteLine("Третьей цифры нет");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Третья цифра "+ number2);
+//     }
+// }
+// thirdnumber();
