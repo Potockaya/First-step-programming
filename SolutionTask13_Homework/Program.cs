59165704391376f2c7339c357398022b7546b9a1
«Marina <marina1904@list.ru>
2022-07-16 19:55:36 +0300

    Add  solution for task 13 Homework

---
+++ SolutionTask13_Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+void thirdnumber()
+{
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    char[] M = inputLine.ToCharArray();
+
+    if (inputLine.Length < 3)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine(M[2]);
+    }
+}
+
+}
+thirdnumber();
