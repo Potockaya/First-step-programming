b660b82aa9a2a2a028a164312fc598b3bc358a52
«Marina <marina1904@list.ru>
2022-07-21 00:20:24 +0300

    Add solution for Task 23 homework

---
+++ SolutionTask23_Homework/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+string? inputLineN = Console.ReadLine();
+if (inputLineN != null)
+{
+    int numberN = int.Parse(inputLineN);
+    string lineN = string.Empty;
+    string lineNcubed = string.Empty;
+    int s = 1;
+    while (s <= numberN)
+    {
+        lineN = lineN + s + "|";
+        lineNcubed = lineNcubed + (s * s * s).ToString() + "|";
+        s++;
+    }
+    Console.WriteLine(new string('-',lineNcubed.Length));
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineNcubed);
+    Console.WriteLine(new string('-',lineNcubed.Length));
+}
