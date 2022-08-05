125efada5266360385d0e66f216e35b2e777aae4
«Marina <marina1904@list.ru>
2022-08-06 00:35:14 +0300

    Correct solution for Task 29_homework

---
+++ SolutionTask29_Homework/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.Write("Введите длину массива!");
+string inputLineNumber = Console.ReadLine() ?? "";
+int inputNumber = int.Parse(inputLineNumber);
+
+Console.Write("Введите начало диапазона значений!");
+string inputDiapasonNumber = Console.ReadLine() ?? "";
+int inputDiapason = int.Parse(inputDiapasonNumber);
+
+Console.Write("Введите конец диапазона значений!");
+string inputDiapasonEndNumber = Console.ReadLine() ?? "";
+int inputDiapasonEnd = int.Parse(inputDiapasonEndNumber);
+
+System.Random numberSintezator = new Random();
+void VariantNaive()
+{
+    int i =0;
+    Console.Write("[");
+    while(i<inputNumber-1)
+    {
+        Console.Write(numberSintezator.Next(inputDiapason,inputDiapasonEnd)+",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(inputDiapason,inputDiapasonEnd));
+    Console.Write("]");
+} 
+VariantNaive();
