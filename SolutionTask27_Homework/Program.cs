fcc4369ae90b77ce9cdecdfb9dfc88abf8ee2058
«Marina <marina1904@list.ru>
2022-08-06 00:11:47 +0300

    Add solution for Task 27_homework

---
+++ SolutionTask27_Homework/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+Console.Write("Введите число!");
+string inputLineNumber = Console.ReadLine()?? "";
+int inputNumber = int.Parse(inputLineNumber);
+int sumOfNumbers;
+
+DateTime timePoint = DateTime.Now;
+
+void VariantSimple()
+{
+    if (inputLineNumber.ToCharArray().Length==3)
+    {
+        int firstDigit= inputNumber/100;
+        int secondDigit= inputNumber/10%10;
+        int thirdDigit= inputNumber%10%10;
+        int sumOfNumbers = firstDigit+secondDigit+thirdDigit;
+        Console.WriteLine(sumOfNumbers);
+    }
+    if (inputLineNumber.ToCharArray().Length==4)
+    {
+        int firstDigit= inputNumber/1000;
+        int secondDigit= inputNumber/100%10;
+        int thirdDigit= inputNumber%100/10;
+        int forthDigit= inputNumber%100%10;
+        int sumOfNumbers = firstDigit+secondDigit+thirdDigit+forthDigit;
+        Console.WriteLine(sumOfNumbers);
+    }
+
+}
+timePoint = DateTime.Now;
+VariantSimple();
+Console.WriteLine(DateTime.Now-timePoint);
+
+
+
