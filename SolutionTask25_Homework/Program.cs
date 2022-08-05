a20fbee9fb79e0796c35974a1a728544c474f076
«Marina <marina1904@list.ru>
2022-08-05 23:09:29 +0300

    Add solution for Task 25_homework

---
+++ SolutionTask25_Homework/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+//Вводные данные
+Console.WriteLine("Введите число!");
+long inputNumber = long.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень!");
+long inputStepen = long.Parse(Console.ReadLine());
+DateTime timePoint = DateTime.Now;
+
+//Метод решения через Math
+void ConculateMath()
+{
+    long result = (long)Math.Pow(inputNumber, inputStepen);
+    Console.WriteLine(result);
+}
+
+//Метод решения через цикл
+void ConculateCircle()
+{
+    long result = 1;
+    for (long i = 1; i <= inputStepen; i++)
+    {
+        result = result * inputNumber;
+    }
+    Console.WriteLine(result);
+}
+// Выводные данные
+timePoint = DateTime.Now;
+ConculateMath();
+Console.WriteLine(DateTime.Now - timePoint);
+
+timePoint = DateTime.Now;
+ConculateCircle();
+Console.WriteLine(DateTime.Now - timePoint);
