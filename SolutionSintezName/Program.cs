b950a498d1eb04223a6be5d8d58cacff469bb74c
«Marina <marina1904@list.ru>
2022-08-06 14:56:43 +0300

    Addt solution for SolutionSintezName

---
+++ SolutionSintezName/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] randoms = new string[6];
+randoms[0] = "Артем";
+randoms[1] = "Михаил";
+randoms[2] = "Игорь";
+randoms[3] = "Виталий";
+randoms[4] = "Илья";
+randoms[5] = "Кирилл";
+Random Name = new Random();
+string randname = randoms[Name.Next(0, randoms.Length + 1)];
+Console.WriteLine(randname);
+
+
+
+
