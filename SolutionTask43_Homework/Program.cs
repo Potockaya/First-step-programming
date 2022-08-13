b2d88c5ba953138697e20f906685c20c2bca214f
«Marina <marina1904@list.ru>
2022-08-13 23:40:46 +0300

    Add solution for SolutionTask43_Homework

---
+++ SolutionTask43_Homework/Program.cs
@@ -0,0 +1,27 @@
+﻿//Ввод 4х глобальных переменных
+Console.WriteLine("Введите коэффициент к1");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффициент к2");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b2");
+double b2 = double.Parse(Console.ReadLine());
+
+// Решение задачи на пересечение прямых
+void Decision()
+{
+    double x = 1;
+    double y = k1 * x + b1;
+    y = k2 * x + b2;
+
+    // (y-b2)/k2=(y-b1)/k1;
+    // k1*(y-b2)=k2*(y-b1);
+    // (k1*y)-(k1*b2)=(k2*y)-(k2*b1);
+    // (k1*y)-(k2*y)=(k1*b2)-(k2*b1);
+    y = (k1 * b2 - k2 * b1) / (k1 - k2);
+    x = (y - b1) / k1;
+    Console.WriteLine("координата y = " + y);
+    Console.WriteLine("координата x = " + y);
+}
+Decision();
