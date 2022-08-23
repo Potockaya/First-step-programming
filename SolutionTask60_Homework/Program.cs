14f95c56e3fa86311ea80964186e29d01b8802ce
«Marina <marina1904@list.ru>
2022-08-23 23:38:00 +0300

    Add solution for SolutionTask60_Homework

---
+++ SolutionTask60_Homework/Program.cs
@@ -0,0 +1,61 @@
+﻿//==========================================================
+//Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+//Напишите программу, которая будет построчно выводить массив,
+//добавляя индексы каждого элемента.
+//==========================================================
+
+// метод для заполнения трехмерного массива из двухзначных чисел
+Console.Clear();
+int[,,] FillThreeDimArray(int sideX, int sideY, int sideZ)
+{
+    System.Random numberSyntezator = new System.Random();
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int[,,] outArray = new int[sideX, sideY, sideZ];
+    while (i < sideX)
+    {
+        j = 0;
+        while (j < sideY)
+        {
+            k = 0;
+            while (k < sideZ)
+            {
+                outArray[i, j, k] = numberSyntezator.Next(10, 100);
+                k++;
+            }
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+//печать трехмерного массива
+void PrintThreeDimArray(int[,,] inputArray)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            k = 0;
+            while (k < inputArray.GetLength(2))
+            {
+                Console.Write($"{inputArray[i, j, k]} ({i},{j},{k}) ");
+                k++;
+            }
+            j++;
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        i++;
+    }
+}
+
+int[,,] threeDimArray = FillThreeDimArray(2, 2, 2);
+PrintThreeDimArray(threeDimArray);
+Console.WriteLine();
