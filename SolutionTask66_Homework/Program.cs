c2867ccd83b383ded761bab7840ed45993af814b
«Marina <marina1904@list.ru>
2022-08-27 21:40:48 +0300

    Add solution for SolutionTask66_Homework

---
+++ SolutionTask66_Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿// //Задача 66: Задайте значения M и N. 
+// //Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+
+int ColculateSum(int M, int N) {
+    if(M > N) {
+        return 0;
+    }
+    return M + ColculateSum(M + 1, N);
+}
+
+int ReadData(string name) {
+    Console.Write($"Введите число {name}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int M = ReadData("M");
+int N = ReadData("N");
+Console.WriteLine($"Сумма чисел от M = {M} до N = {N} равна {ColculateSum(M, N)}");
